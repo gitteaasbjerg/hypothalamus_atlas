c6a3c11235cbc20ab493d39a0e35e1ec01d992db
pvtodorov <petar.v.todorov@gmail.com>
2018-10-03 15:08:14 +0200

    create folders, download data
    - if Data and Outputs folder are created if they don't exist
    - if mouse_matrix.h5 is not available, it is downloaded

--- Scripts/run_pipeline.sh
+++ Scripts/run_pipeline.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+if [[ ! -e /Data/ ]]; then
+    mkdir -p Data
+    cd Data
+    wget https://s3.amazonaws.com/mssm-seq-matrix/mouse_matrix.h5
+    cd ..
+fi
+if [[ ! -e /Ouputs/ ]]; then
+    mkdir -p Outputs
+fi
 Rscript Scripts/get_sample_ids_from_meta.R Settings/get_sample_ids_from_meta.yaml 
 Rscript Scripts/get_gene_idxs.R Outputs/sample_idx_from_meta.yaml 3
 Rscript Scripts/WGCNA_outlier_removal.R
