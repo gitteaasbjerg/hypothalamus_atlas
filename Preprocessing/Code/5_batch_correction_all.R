eb70cd7544cc33d67ea3e1d1bca19eed4a77b02a
Gitte Aasbjerg <gitte.aasbjerg@hotmail.com>
2020-01-17 14:16:10 +0100

    Error corrections, add PCA plots, change pipeline
    Minor error corrections.
    Add PCA plot before and after batch correction 
    Change pipeline so study_id is the only batch for complete csv file

--- Preprocessing/Code/5_batch_correction_all.R
+++ Preprocessing/Code/5_batch_correction_all.R
@@ -1,7 +1,9 @@
 #!/usr/bin/env Rscript
 library('limma')
 library("yaml")
-warning=FALSE
+library('ggplot2')
+library('ggrepel')
+#warning=FALSE
 yaml_path <- '/projects/gitte/hypothalamus_atlas/get_sample_ids_from_meta.yaml'
 y <- read_yaml(yaml_path)
 studies <- y[['studies']]
@@ -11,16 +13,29 @@ cat('------------------------------------------------\n  Removing batch effects 
 data = read.csv(y[['output']][['data_path']],row.names = 1, header = TRUE)
 meta = read.csv(y[['output']][['meta_path']],row.names = 1, header = TRUE)
 
+#Initiate batch correction
 batches <- y[['batches']]
 covariets <- y[['covariates']]
 
+
+#If no batches are given use 'Sample_series_id' as default
+if (is.null(batches)){
+  batches <- 'Sample_series_id'
+}
+
+#Create PCA before batch correction
+pca <- as.data.frame(prcomp(t(data),rank=2)$x)
+pca$study <- as.character(meta[batches][,1])
+p <- ggplot(pca,aes(x=PC1,y=PC2,color=study)) + geom_point() 
+ggsave(paste0(y[['output']][['output_path']],'/PCA_pre_batch_correction','.pdf'), p, width = 10, height=10)
+
+
 #Extract columns & rows relevant for batch correction
-sample_covariets <- meta[,covariets]
-sample_batches <- meta[,batches]
+sample_covariets <- meta[covariets]
+sample_batches <- meta[batches]
 
 #Remove batches with only one distinct value
-sample_batches <- sample_batches[, sapply(sample_batches, function(col) length(unique(col))) > 1]
-
+sample_batches <- sample_batches[sapply(sample_batches, function(col) length(unique(col))) > 1]
 
 batch <- list()
 bb    <- list()
@@ -32,11 +47,9 @@ if (length(sample_batches) > 0){
     bb            <- droplevels(sample_batches[[b]])
     contrasts(bb) <- contr.sum(levels(bb))
     batch[[b]]    <- bb
-  }
-  
-  #Create batch input-string for model matrix
-  for (i in 1:length(sample_batches)){
-    model_matrix_b_input <- paste0(model_matrix_b_input,'batch[[',i,']]+')
+    
+    #Create batch input-string for model matrix
+    model_matrix_b_input <- paste0(model_matrix_b_input,'batch[[',b,']]+')
   }
 }
 
@@ -62,5 +75,12 @@ if (model_matrix_input == ''){
   data_corrected  <- removeBatchEffect(data, covariates=cova, design=design_matrix)
 }
 
-write.csv(data_corrected, y[['output']][['data_path']], quote=F)
+#Create PCA after batch correction
+pca1 <- as.data.frame(prcomp(t(data_corrected),rank=2)$x)
+pca1$study <- as.character(meta['Sample_series_id'][,1])
+p <- ggplot(pca1,aes(x=PC1,y=PC2,color=study)) + geom_point() 
+ggsave(paste0(y[['output']][['output_path']],'/PCA_batch_corrected','.pdf'), p, width = 10, height=10)
+
+
+write.csv(as.data.frame(data_corrected), y[['output']][['data_path']], quote=F)
 print(paste0("Batch correction of ", length(sample_batches)," batches and ", length(sample_covariets), ' covariates has been successfully conducted on all ', length(studies), ' studies'))
